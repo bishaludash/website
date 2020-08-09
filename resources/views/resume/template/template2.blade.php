8ea06f3f2a556d66d70eb67d0245e5fa023dfa0e
Bishal Udash <supertramp.bishal@gmail.com>
2020-08-09 15:27:52 +0545

    second theme complete

--- resources/views/resume/template/template2.blade.php
+++ resources/views/resume/template/template2.blade.php
@@ -1,110 +1,175 @@
-<style>
-    .intro-wrapper, .details-wrapper{
+<style>    
+
+    .section-header{
+        font-size: 1.5em;
+        font-weight: 600;
+        line-height: 2;
+    }
+    
+    .section-header::after {
+        content: " ";
+        display: block;
+        height: 3px;
+        background: #9ab4ca;
+    }
+    
+    
+    .resume-img, .details-small{
+        padding: 1%;
+        width: 25%;
+        float: left;
+    }
+    
+    .user_image{
+        width: 100%;
+        height: auto;
+    }
+    
+    .resume-user-name{
+        background: #353332;
+        color: #fff;
+        padding: 10px;
+        font-size: 1.5rem;
+        letter-spacing: 1.5px;
+    }
+    
+    .contact{
+        list-style: none;
+        padding: 0;
+    }
+    
+    .info{
         display: flex;
     }
     
-    .dp-wrapper, .details-small{
-        flex: 2.3;
+    /* school and jobs */
+    .school, .job{
+        margin-bottom: 25px;
+        padding: 10px;
+    }
+    .school-info, .job-info{
+        font-weight: bold;
+        flex:4;
+        width: 80%;
+        text-transform:capitalize
+    }
+    .school-year, .job-year{
+        float: right;
+        flex: 1;
+    }
+    .info-secondary{
+        margin-bottom: 10px;
     }
     
-    .summary, .details-large{
-        flex: 6;
-    }
     
-    .dp-wrapper{
-        /* width: 200px; */
-        height: auto;
-        background-position: center;
-        background-size: cover;
-        background-repeat: no-repeat;
-        border-radius: 1%;
-    }
-
-    .summary, .details-small{
-        background-color: #e3e3e3;
-    }
-
-    .user-name{
-        color: #fff;
-        padding: 8px 15px;
-        word-spacing: 2px;
-        letter-spacing: 1.5px;
-    }
-
-    .list-item{
-        line-height: 1;
-    }
-    
+}
 </style>
 
-{{-- html --}}
-{{-- {{dd($resume)}} --}}
-<div class="intro-wrapper">
-    <div class="dp-wrapper" style="background: url({{asset($resume['avatar'])}}); 
-    background-position: center; 
-    background-size: cover;
-    background-repeat: no-repeat; ">
-    {{-- <img src="{{asset($resume['avatar'])}}" class="img-fluid" alt=""> --}}
-</div>
-<div class="summary">
-    <p class="px-5 pt-5">
-       {{$resume['user_summary']}}
-    </p>
-</div>
-</div>
-
-{{-- Name --}}
-<div class="user-name bg-dark">
-    <h3>
-        {{sprintf("%s %s", $resume['first_name'], $resume['last_name'])}}
-    </h3>
+{{-- Avatar and summary --}}
+<div class="section" style="background: #e3e3e3; margin-bottom:0" >
+    {{-- avatar section --}}
+    @if (!is_null($resume['avatar']) && $resume['avatar'] != "")
+    <div class="resume-img">
+        <img src="{{asset($resume['avatar'])}}" class="user_image" alt="">
+    </div>
+    @endif
+    
+    {{-- summary --}}
+    <div style="float: left; width:70%; padding:1% 3%;">
+        {!! $resume['user_summary'] !!}
+    </div>
+    
+    <br style="clear: left;" />
 </div>
 
-<div class="details-wrapper border">
+
+{{-- Resume user name --}}
+<div class="resume-user-name">
+    {{ sprintf("%s %s", $resume['first_name'], $resume['last_name'])  }}
+</div>
+
+
+{{-- Contact and skills --}}
+<div class="section">
+    {{-- Contact --}}
     <div class="details-small">
-        <ul class="contact list-unstyled m-4">
-            <li class="pb-3 list-item"><strong>Phone :</strong>  <br>
+        <ul style="list-style:none; padding:0">
+            <li style="line-height: 1; margin-bottom: 10px;">
+                <strong>Phone : </strong>  <br>
                 {{$resume['phone']}}
             </li>
-
-            <li class="pb-3 list-item"><strong>Email :</strong>  <br>
+            <li style="line-height: 1; margin-bottom: 10px;">
+                <strong>Email : </strong>  <br>
                 {{$resume['email']}}
             </li>
-
-            <li class="pb-3 list-item"><strong>Address :</strong>  <br>
-                {{$resume['city']}}<br>
-                {{$resume['state_province']}}
+            <li style="line-height: 1; margin-bottom: 10px; text-transform:capitalize">
+                <strong>Location : </strong>  <br>
+                {{ sprintf("%s %s", $resume['state_province'] ?? null, $resume['city']?", ".$resume['city'] : null) }}
             </li>
-
-            <li class="pb-3 list-item"><strong>Social :</strong>  <br>
-                link
+            <li style="line-height: 1; margin-bottom: 10px;">
+                <strong>Social : </strong>  <br>
+                {{$resume['phone']}}
             </li>
         </ul>
-        
-        <ul class="skills list-unstyled m-4">
-            <strong>Skills</strong> <br>
-            {!! $resume['skills']!!}
-        </ul>
     </div>
-    <div class="details-large">
-        <div class="mx-5 my-4">
-            <strong>Experience</strong>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
+    
+    
+    {{-- Skills --}}
+    <div style="float: left; width:70%; padding:1% 3%;">
+        {{-- Skills and hobby --}}
+        <strong>Skills</strong>
+        <div class="skills">
+            {!!$resume['skills']!!}
         </div>
         
-        <hr>
-        
-        <div class="mx-5 my-4">
-            <strong>Education</strong>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate mollitia quibusdam nam tenetur cumque provident quaerat excepturi nostrum, natus doloremque!</li>
-            
+    </div>
+    
+    <br style="clear: left;" />
+</div>
+
+
+{{-- Education --}}
+<div class="section">
+    <p class="resume-user-name">Education</p>        
+    @foreach ($resume['school'] as $school)
+    {{-- {{dd($school)}} --}}
+    <div class="school">
+        <div class="info">
+            <div class="school-info">{{ sprintf("%s ( %s )", $school['school_name'], $school['school_location'])  }}</div>
+            <div class="school-year">{{sprintf("%s - %s", $school['edu_start_year'], $school['edu_end_year'])}}</div>
+        </div>
+        <div class="info-secondary">
+            <span >{{ sprintf("%s, %s", $school['degree'], $school['field_of_study'])  }}</span>
+        </div>
+        <div class="achievements">
+            {!!$school['achievements']!!}
+        </div>
+    </div> 
+    @endforeach
+</div>
+
+{{-- Experience --}}
+<div class="section">
+    <p class="resume-user-name">Experience</p>
+    
+    @foreach ($resume['jobs'] as $job)
+    {{-- {{dd($job)}} --}}
+    <div class="job">
+        <div class="info">
+            <div class="job-info">{{ sprintf("%s ( %s )", $job['job_employer'], $job['job_city']) }}</div>
+            <div class="job-year">
+                {{sprintf("%s - %s", 
+                date('M Y', strtotime($job['job_start_date'])),
+                $job['job_end_date']== ("" || null)? 'Present' : date('M Y', strtotime($job['job_end_date'])))}}
+            </div>
+        </div>
+        <div class="info-secondary">
+            <span>{{ $job['job_title']}} </span>
+        </div>
+        <div class="achievements">
+            {!! $job['job_details']!!}
         </div>
     </div>
+    @endforeach
+    
 </div>
